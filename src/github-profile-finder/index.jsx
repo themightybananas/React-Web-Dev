9d783bbd543a7d8e6e5b375814e7d38774c9571f
themightybananas <pancahisam18@gmail.com>
2024-09-28 19:39:44 +0700

    github-profile-finder

---
+++ src/github-profile-finder/index.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import User from "./user";
+import "./styles.css";
+
+
+export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("themightybananas");
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  async function fetchGithubUserData() {
+    setLoading(true);
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+    if (data) {
+      setUserData(data);
+      setLoading(false);
+      setUserName('');
+    }
+  }
+
+  function handleSubmit() {
+    fetchGithubUserData();
+  }
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading data please wait</div>;
+  }
+
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Search Github Username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+      {userData !== null ? <User user={userData} /> : null}
+    </div>
+  );
+}
